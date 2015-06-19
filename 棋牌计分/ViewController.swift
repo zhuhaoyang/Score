de8caad88081d196aea14fe1e0056bd73be41ee7
peter <peter@hejiajinrong.com>
2015-06-19 14:26:18 +0800

    Initial Commit

---
+++ 棋牌计分/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  棋牌计分
+//
+//  Created by Myth on 15/6/19.
+//  Copyright (c) 2015年 Myth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
